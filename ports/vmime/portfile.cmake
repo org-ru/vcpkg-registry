ae26dafa6fdcb42873a7e195e44ea6e5543c1d86
markthomas <markthomas@communigate.com>
2024-09-19 14:56:03 +0400

    update vmime portfile

--- ports/vmime/portfile.cmake
+++ ports/vmime/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO org-ru/vmime
     REF v0.9.2
-    SHA512 0
+    SHA512 13123b635750c1de46a6df185d1f5f8453bb76fb08d574ac98357e467ef21d44f9723412cdb58c9e203bd038c8a3a43168db68da940255e2c10346b576468ab5
     HEAD_REF master
 )
 
