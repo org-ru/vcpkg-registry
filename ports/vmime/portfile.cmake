72539a35abda3f690e3e2b544cb21f7ad7c40d7a
markthomas <markthomas@communigate.com>
2024-09-20 19:38:56 +0400

    2

--- ports/vmime/portfile.cmake
+++ ports/vmime/portfile.cmake
@@ -29,6 +29,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/vmime")
+message("lib/cmake/vmime")
 
-vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/vmime")
